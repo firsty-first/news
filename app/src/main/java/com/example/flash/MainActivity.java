65763c7eeb81ac7e567a61fbc42274b15add3f86
Gaurav Singh <gks.21u10252@btech.nitdgp.ac.in>
2023-06-01 04:23:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/flash/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.flash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+}}
